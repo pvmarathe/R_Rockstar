6526938c1255e30c4c7f5e1334a7a75311341381
Looking For Inspiration <prasad.marathe@outlook.com>
2017-12-09 21:34:00 -0500

    Update R_Rockstar.R
    R Practice Codes

--- R_Rockstar.R
+++ R_Rockstar.R
@@ -3,6 +3,6 @@
 
 getwd()
 
-# 2. Changes the Working Directory
+# 2. Changes the Working Directory. Set the default working directory to be the one where you have saved your datasets
 
 setwd("C:/myfolder/data")
