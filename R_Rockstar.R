c993ba4ff1a9560b3f7d6fd381632427c93fc36e
Looking For Inspiration <prasad.marathe@outlook.com>
2017-12-09 23:28:06 -0500

    Update R_Rockstar.R

--- R_Rockstar.R
+++ R_Rockstar.R
@@ -6,3 +6,5 @@ getwd()
 # 2. Changes the Working Directory. Set the default working directory to be the one where you have saved your datasets
 
 setwd("C:/myfolder/data")
+
+# 3. The Global Environment. The global environment is the interactive workspace. This is the environment where one actually works
